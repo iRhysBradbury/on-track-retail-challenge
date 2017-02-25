de20d370ef1a7c7941e932cbbf7f206578299413
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-02-25 01:12:12 +0000

    adding changes to Sequence.increment

--- src/main/scala/com/ontrackretail/codechallenge/Sequence.scala
+++ src/main/scala/com/ontrackretail/codechallenge/Sequence.scala
@@ -1,13 +1,27 @@
 package com.ontrackretail.codechallenge
 
+import scala.annotation.tailrec
+
 object Sequence {
 
-  def increment(seq: Seq[Int]): Seq[Int] = {
+  @tailrec
+  def increment(seq: Seq[Int], num: Int = 0, steps: Int = 0): Seq[Int] = {
     seq match {
-      case Nil => Nil
-      case _ => {
-        (Integer.valueOf(seq.mkString) + 1).toString.toCharArray.map(x => Integer.valueOf(x.toString).toInt).toSeq
-      }
+      case head :: tail => increment(
+        tail,
+        num + head * math.pow(10, seq.length - 1).toInt,
+        steps + 1
+      )
+      case Nil => if (steps == 0) Seq.empty else asSeq(num + 1)
+    }
+  }
+
+  @tailrec
+  def asSeq(num: Int, res: Seq[Int] = Seq.empty): Seq[Int] = {
+    if (num > 0) {
+      asSeq(num / 10, res :+ (num % 10))
+    } else {
+      res.reverse
     }
   }
 }
